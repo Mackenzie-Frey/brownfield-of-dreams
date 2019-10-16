5927116cc26d7851025cb1c869f38dede394cd36
Alec Wells <alec.wells47@gmail.com>
2019-10-16 17:12:38 -0600

    Add functionailty to admin destroy tutorial button

--- spec/models/tutorial_spec.rb
+++ spec/models/tutorial_spec.rb
@@ -3,4 +3,12 @@
 require 'rails_helper'
 
 RSpec.describe Tutorial, type: :model do
+  describe 'class methods' do
+    it 'open_content' do
+      tut_1 = create(:tutorial, classroom: false)
+      tut_2 = create(:tutorial, classroom: true)
+
+      expect(Tutorial.open_content).to eq([tut_1])
+    end
+  end
 end
