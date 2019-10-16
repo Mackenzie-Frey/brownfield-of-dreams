0c636cd2f1864f5df07de909e6ce3be4304ef033
Alec Wells <alec.wells47@gmail.com>
2019-10-16 17:51:29 -0600

    Update test coverage
    Co-authored-by: Mack Halliday <16658577+MackHalliday@users.noreply.github.com>

---
+++ spec/features/admin/new_tutorial_spec.rb
@@ -0,0 +1,24 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+describe 'An Admin can do things to tutorial' do
+  it 'create' do
+    admin = create(:user, role: 1)
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(admin)
+
+    visit new_admin_tutorial_path
+    click_on 'Save'
+
+    expect(page).to have_content("Title can't be blank, Description can't be blank, and Thumbnail can't be blank")
+
+    fill_in 'tutorial[title]', with: 'How to tie your shoes.'
+    fill_in 'tutorial[description]', with: 'Over, under, around and through, Meet Mr. Bunny Rabbit, pull and through.'
+    fill_in 'tutorial[thumbnail]', with: 'J7ikFUlkP_k'
+
+    click_on 'Save'
+
+    tutorial = Tutorial.last
+    expect(tutorial.title).to eq('How to tie your shoes.')
+  end
+end
