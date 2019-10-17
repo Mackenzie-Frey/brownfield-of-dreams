8565433cdff3ce13e8ee90f2ddcf65dce93760f4
MackHalliday <mackenzie.halliday@gmail.com>
2019-10-16 21:47:57 -0600

    Removed validation for thumbnail :

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,4 +1,3 @@
-# frozen_string_literal: true
 
 class Video < ApplicationRecord
   has_many :user_videos
@@ -7,7 +6,6 @@ class Video < ApplicationRecord
 
   validates_presence_of :title
   validates_presence_of :description
-  validates_presence_of :thumbnail
 
   def self.vidoes_by_tutorial
     order('position ASC').group_by(&:tutorial_id)
