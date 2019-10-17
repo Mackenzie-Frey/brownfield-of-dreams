4b17930d6f5eca6aefd823bca30a753b6f195658
MackHalliday <mackenzie.halliday@gmail.com>
2019-10-16 18:30:04 -0600

    test for visiting start page

--- spec/features/vistors/visitor_can_view_get_started_spec.rb
+++ spec/features/vistors/visitor_can_view_get_started_spec.rb
@@ -4,7 +4,6 @@ describe 'visitor visits get started page' do
   it 'can view get started page' do
 
     visit get_started_path
-    
 
     expect(page).to have_content("Get Started")
     expect(page).to have_content("Browse tutorials from the homepage.")
